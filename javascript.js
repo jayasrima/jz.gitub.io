e46d0d6b694e5a9c4ffcb94e9e0ab1b90dc3e8cb
jayasrima <40634456+jayasrima@users.noreply.github.com>
2018-06-27 10:01:31 -0400

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,14 @@
+function a(){
+  window.alert("Easier Tricks");
+
+}
+
+function b(){
+  window.location.href= "https://www.blogger.com/blogger.g?blogID=4787907883289727434#publishedposts/";
+}
+function 25(){
+
+}
+function 50(){
+  
+}
